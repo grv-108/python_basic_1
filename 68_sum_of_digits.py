7cb4adab04b8fbfefe2fa5291dac32efc5ea1cd7
Gaurav <gauravsubhash9999@gmail.com>
2025-09-13 19:04:21 +0530

    solved ex 68

--- 68_sum_of_digits.py
+++ 68_sum_of_digits.py
@@ -5,8 +5,22 @@
 def getUserInput():
     return int(input("Enter a digit --> ").strip())
 
+def sumOfDigit(number):
+    number = abs(number)
+    total = 0
+    while number > 0:
+        digit = number % 10
+        total += digit
+        number //=10
+    return total
+    
 def main():
-    digit = getUserInput()
-    temp = 0
-    while len(digit) != 0:
-        
+    try:
+        number = getUserInput()
+        result = sumOfDigit(number)
+        print(f"Sum of {number} : {result}")
+    except ValueError:
+        print(f"Invalid number!!!")
+    
+if __name__ == "__main__":
+    main()
